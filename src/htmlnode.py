9c42ac2e4eef4348b1b315fe519629233f3bd7dc
Neal Powers <neal.grindstaff@gmail.com>
2025-03-31 15:48:36 -0400

    add leafnode and fix test error

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -16,6 +16,19 @@ class HTMLNode:
         if self.props is None:
             return attribute_string
         for key, value in self.props.items():
-            attribute_string += f'{key}="{value}" '
-        return attribute_string.strip()
-    
+            attribute_string += f' {key}="{value}"'
+        return attribute_string
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("LeafNode value cannot be None")
+        if self.tag is None:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+        
+    def __repr__(self):
+        return f'LeafNode(Tag:{self.tag}, Value:{self.value}, Props:{self.props})'
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import *
 
 class TestTextNode(unittest.TestCase):
     def test_repr(self):
@@ -12,7 +12,15 @@ class TestTextNode(unittest.TestCase):
         node = HTMLNode("div", "This is a div node")
         self.assertEqual(node.props_to_html(), "")
         node2 = HTMLNode("div", "This is a div node", None, {"class": "hi-there", "href": "https://www.google.com"})
-        self.assertEqual(node2.props_to_html(), 'class="hi-there" href="https://www.google.com"')
+        self.assertEqual(node2.props_to_html(), ' class="hi-there" href="https://www.google.com"')
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+    
+    def test_leaf_to_html_div(self):
+        node = LeafNode("div", "Hello, world!", {"class": "hi-there"})
+        self.assertEqual(node.to_html(), '<div class="hi-there">Hello, world!</div>')
 
 if __name__ == "__main__":
     unittest.main()
