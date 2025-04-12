2336a6a2b9796dc24717136dd0373abeb16a3a73
Neal Powers <neal.grindstaff@gmail.com>
2025-04-11 23:23:18 -0400

    rename file

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -2,7 +2,6 @@ import unittest
 
 from textnode import TextNode, TextType
 
-
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
         node = TextNode("This is a text node", TextType.BOLD)
@@ -24,7 +23,11 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a bold node", TextType.BOLD)
         self.assertNotEqual(node, node2)
 
-
-
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+        
 if __name__ == "__main__":
     unittest.main()
